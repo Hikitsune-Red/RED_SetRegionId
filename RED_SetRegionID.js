c529b50dff4716a0c271ddafb53704fceb4d4fb2
Hikitsune-Red <hikitsune.red@gmail.com>
2016-10-24 11:37:04 -0600

    Add files via upload

--- RED_SetRegionID.js
+++ RED_SetRegionID.js
@@ -1,5 +1,5 @@
 /*:
- * @plugindesc v1.00 A simple plug-in designed to allow the setting of Region ID's in-game
+ * @plugindesc v1.01 A simple plug-in designed to allow the setting of Region ID's in-game
  * @author Hikitsune-Red 火狐
  *
  * @help
@@ -21,9 +21,67 @@
  * Free for any commercial or non-commercial project!
  * Just credit Hikitsune-Red 火狐 in your project
  */
-
-Game_Map.prototype.setTileId = function(x, y, regId) {
-    var width = $dataMap.width;
-    var height = $dataMap.height;
-    $dataMap.data[(5 * height + y) * width + x] = (regId || 0);
-};
+ 
+(function() {
+	
+	var _Game_System_initialize = Game_System.prototype.initialize;
+	Game_System.prototype.initialize = function()
+	{
+		_Game_System_initialize.call(this);
+		this.initREDRegions();
+	};
+	
+	Game_System.prototype.initREDRegions = function()
+	{
+		this._REDRegions = {};
+	};
+	
+	Game_System.prototype.REDRegions = function()
+	{
+		if (this._REDRegions === undefined) this.initREDRegions();
+		return this._REDRegions;
+	};
+	
+	Game_System.prototype.getREDRegion = function(mapId, x, y, z)
+	{
+		if (this._REDRegions === undefined) this.initREDRegions();
+		var width = $dataMap.width;
+		var height = $dataMap.height;
+		return this._REDRegions[mapId, (z * height + y) * width + x];
+	};
+	
+	Game_System.prototype.isREDRegion = function(mapId, x, y, z)
+	{
+		if (this._REDRegions === undefined) this.initREDRegions();
+		var width = $dataMap.width;
+		var height = $dataMap.height;
+		return this._REDRegions[mapId, (z * height + y) * width + x] !== undefined;
+	};
+	
+	Game_System.prototype.setREDRegion = function(mapId, x, y, z, regId)
+	{
+		console.log('Setting region id of Map:' + mapId + ' x:' + x + ' Y:' + y + ' to REG:' + regId);
+		var width = $dataMap.width;
+		var height = $dataMap.height;
+		this._REDRegions[mapId, (z * height + y) * width + x] = (regId || 0);
+	};
+	
+	Game_Map.prototype.setTileId = function(x, y, regId) 
+	{
+		$gameSystem.setREDRegion($gameMap._mapId, x, y, 5, regId);
+	};
+	
+	var _Game_Map_tileId = Game_Map.prototype.tileId;
+	Game_Map.prototype.tileId = function(x, y, z)
+	{
+		if ($gameSystem.isREDRegion($gameMap._mapId, x, y, z))
+		{
+			var width = $dataMap.width;
+			var height = $dataMap.height;
+			return $gameSystem.getREDRegion($gameMap._mapId, x, y, z);
+		} else {
+			return _Game_Map_tileId.call(this, x, y, z);
+		}
+	};
+	
+}) ();
